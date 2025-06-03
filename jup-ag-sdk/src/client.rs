557dc1140c997218e7c783dfc302011ca85b3920
thrishank <thrishankkalluru@gmail.com>
2025-06-03 16:08:32 +0530

    add api key function and add headers

--- jup-ag-sdk/src/client.rs
+++ jup-ag-sdk/src/client.rs
@@ -1,4 +1,7 @@
-use reqwest::Client;
+use reqwest::{
+    Client,
+    header::{HeaderMap, HeaderValue},
+};
 
 /// `JupiterClient` is a client wrapper to interact with the Jupiter Aggregator APIs.
 /// It is your gateway to interact with the Jupiter exchange API
@@ -13,7 +16,7 @@ impl JupiterClient {
     ///
     /// # Arguments
     ///
-    /// * `base_url` - Base URL for the Jupiter API, typically `https://lite-api.jup.ag/swap/v1`.
+    /// * `base_url` - Base URL for the Jupiter API, typically `https://lite-api.jup.ag`. for pro api use https://api.jup.ag
     ///
     /// # Example
     ///
@@ -21,12 +24,47 @@ impl JupiterClient {
     /// let api = JupiterClient::new("https://lite-api.jup.ag");
     /// ```
     pub fn new(base_url: &str) -> Self {
-        let client = Client::new();
+        let mut headers = HeaderMap::new();
+        headers.insert("Accept", "application/json".parse().unwrap());
+        headers.insert("Content-Type", "application/json".parse().unwrap());
+
+        let client = Client::builder()
+            .default_headers(headers)
+            .build()
+            .expect("Failed to build client with API key");
+
+        // let client = Client::new();
         JupiterClient {
             client,
             base_url: base_url.to_string(),
-            // TODO: Add Api key here
-            // make the base_url default
+        }
+    }
+
+    /// Returns a new JupiterClient with the API key set in headers.
+    ///
+    /// # Arguments
+    ///
+    /// * `api_key` - your api key, you can get one from here `https://portal.jup.ag/onboard`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let api = JupiterClient::new("https://api.jup.ag").with_api_key('your-api-key');
+    /// ```
+    pub fn with_api_key(self, api_key: &str) -> Self {
+        let mut headers = HeaderMap::new();
+        headers.insert("x-api-key", HeaderValue::from_str(api_key).unwrap());
+        headers.insert("Accept", "application/json".parse().unwrap());
+        headers.insert("Content-Type", "application/json".parse().unwrap());
+
+        let client = Client::builder()
+            .default_headers(headers)
+            .build()
+            .expect("Failed to build client with API key");
+
+        JupiterClient {
+            client,
+            base_url: self.base_url,
         }
     }
 }
